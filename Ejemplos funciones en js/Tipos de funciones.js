dfffa5a7d26af130c95318b7652e7d670c81b5ba
Juan Felipe <juan57joaqui@gmail.com>
2025-02-03 13:25:42 -0500

    Primer commit con archivos iniciales

---
+++ Ejemplos funciones en js/Tipos de funciones.js
@@ -0,0 +1,8 @@
+    var prompt = require("prompt-sync")()
+
+    const nombre = prompt("Ingrese su nombre completo ")
+    
+    console.log("Hola " + nombre + ", bienvenido a mi programa")
+
+
+
---
+++ Ejemplos funciones en js/timeout.js
@@ -0,0 +1,8 @@
+
+let saludar = () => {
+    console.log("Hola Mundo")
+}
+    setTimeout(saludar, 3000)
+
+    saludar()
+
